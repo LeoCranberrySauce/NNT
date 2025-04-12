ddc986592678f3a2a043714e9185c364e8cc2222
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-12 10:54:29 +0800

    creating some menu items

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,18 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu = () => {
   return (
     <div className='explore-menu' id='explore-menu'>
-      <h1></h1>
+      <h1>Explore our foods</h1>
+      <p className='explore-menu-text'>Choose food...</p>
+      <div className='explore-menu-list'>
+        {menu_list.map(()=>{
+          return (
+            <div key={index} className='explore-menu-list-item'>
+              <img src={item.menu_item} alt=""/>
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
