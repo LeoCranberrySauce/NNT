a065333405ac6e580d99329285e6c657d7f31d4b
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-12 09:57:52 +0800

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h1>Welcome to NNT Purple Food House</h1>
+        <p>Satisfy your cravings!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
