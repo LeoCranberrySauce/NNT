0e6050ee670fa3092c1d1a3a306a8750870ae289
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-17 11:24:48 +0800

    We will fix this later

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
+//import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://cranberrysauce:Lbn02082001@cluster0.w5yrgev.mongodb.net/nnt-food-del').then(()=>console.log("DB Connected"));
-}
+//export const connectDB = async () => {
+//    await mongoose.connect('mongodb+srv://cranberrysauce:Lbn02082001@cluster0.w5yrgev.mongodb.net/nnt-food-del').then(()=>console.log("DB Connected"));
+//}
