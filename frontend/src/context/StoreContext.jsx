d539574562d1662f999182517ad765a61e825119
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-23 11:10:46 +0800

    I already finished the fetch-new-data branch because I want to add the new data to the cart from the da

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,14 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
-    const [token, setToken] = useState("")
+    const url = "http://localhost:4000";
+    const [token, setToken] = useState("");
+    const [food_list, setFoodList] = useState([])
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -27,16 +28,26 @@ const StoreContextProvider = (props) => {
         for (const item in cartItems) {
             if (cartItems[item] > 0) {
                 let itemInfo = food_list.find((product) => product._id === item);
-                totalAmount += itemInfo.price * cartItems[item];}
+                totalAmount += itemInfo.price * cartItems[item];
+            }
         }
         return totalAmount;
     }
 
-    useEffect(()=>{
-        if(localStorage.getItem("token")){
-            setToken(localStorage.getItem("token"));
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list");
+        setFoodList(response.data.data);
+    }
+
+    useEffect(() => {
+        async function loadData(){
+            await fetchFoodList();
+            if(localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"));
+            }
         }
-    },[])
+        loadData();
+    }, [])
 
     const ContextValue = {
         food_list,
