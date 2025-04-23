ab8f0059611facce70ca5ddc8f2465e21c77daf3
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-23 11:23:50 +0800

    I created the middleware and controllers for the cart functionality.

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,18 @@
+import userModel from "../models/userModel.js";
+
+// Add to cart
+const addToCart = async (req,res) => {
+
+}
+
+// Remove from cart
+const removeFromCart = async (req,res) => {
+
+}
+
+// Get cart items
+const getCartItems = async (req,res) => {
+
+}
+
+export {addToCart,removeFromCart,getCartItems};
