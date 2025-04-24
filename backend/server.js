d54ef9123a495aea9b719d99bfb3f83032530983
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-24 17:55:14 +0800

    Create Place Order and Stripe Payment

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-import cartRouter from "./routes/cartRoute.js"
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 
@@ -24,6 +25,7 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 // api routes
 app.get("/", (req, res) => {
