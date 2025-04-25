1e228004019e53567e6cf8fdcd4f05d32e69a79b
LeoCranberrySauce <leandrongayawen8888@gmail.com>
2025-04-25 10:55:39 +0800

    Create cart functionality (#43)
    * creating some menu items
    
    * I have created category from UI, and partially created menu items.
    However, I will replace some food items for the next time.
    
    * I created some basic footer of my UI website, but it is not yet implemented.
    
    * Finally finished the basic UI design of the footer, though real-time coding will do on the next sessiom.
    
    * Minor changes: Text for the Header
    
    * Now finally finished a website responsive.
    
    * I finally finished my Login / Signup popup UI.
    
    * I created the "Add to Cart" and "Payment Center" UI, with a little modification of Navigation bar UI
    
    * I finally create the backend of the website
    
    * Create a partial database
    
    * We will fix this later
    
    * I finally tested the API with MongoDB!
    
    * I will create a new admin panel for the food ordering system
    
    * I mistakenly deleted the admin panel files into backend folder instead of MAIN FOLDER.
    
    * I mistakenly deleted the admin panel files into backend folder INSTEAD
    
    * I added add, list and orders pages from admin panel, however when i click the sidebar options, the page doesn't change color. I will fix some bugs about the hover effect.
    
    * Now finally added the Add Items page of the Admin panel, and tried to test if I add one item and then successfully placed on my database.
    
    * I finished the admin panel but I will create user authentication later
    I tried testing the Food List and Add Food using API so I successfully tested it
    
    * I successfully implemented the testing of the user authentication. Now I will create the frontend part of both Login and Register pages.
    
    * I finally implemented the user authentication. Now I will do fetching the data from the backend.
    
    * I already finished the fetch-new-data branch because I want to add the new data to the cart from the da
    
    * I created the middleware and controllers for the cart functionality.
    
    * I created the cart functionality. Today, I've added the cart functionality to the frontend and trying to test the API.
    However, I'm facing some issues with the API functionality, but luckily I've finally debugged the issues partially.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
 import 'dotenv/config'
 
 
@@ -22,10 +23,11 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 // api routes
 app.get("/", (req, res) => {
-     res.send("API workingworek")
+     res.send("API working")
 });
 
 app.listen(port, () => {
